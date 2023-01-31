e47e30d9f0b034047b45756bc4fb6fda5486bac3
Ventulus <everywherewind@163.com>
2023-02-01 00:28:31 +0800

    MoreNPCInfo

---
+++ MoreNPCInfo/MoreNPCInfo/Class1.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Fungus;
+using HarmonyLib;
+//using KBEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//using KBEngine;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+namespace Ventulus
+{
+    [BepInPlugin("Ventulus.MCS.MoreNPCInfo", "MoreNPCInfo", "1.0")]
+    public class MoreNPCInfo : BaseUnityPlugin
+    {
+        void Start()
+        {
+            //输出日志
+            Logger.LogInfo("更多NPC信息加载成功！");
+            var harmony = new Harmony("Ventulus.MCS.MoreNPCInfo");
+            harmony.PatchAll();
+
+        }
+
+        public static MoreNPCInfo Instance;
+        void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
