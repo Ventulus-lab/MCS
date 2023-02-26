4072a82238544b1077a742b7cf8901b73727ee3e
Ventulus <everywherewind@163.com>
2023-02-27 00:45:45 +0800

    可使用“BepInEx插件管理器”F1键修改每回合消耗的天数了

--- BigTuPoSpendTime/BigTuPoSpendTime/Class1.cs
+++ BigTuPoSpendTime/BigTuPoSpendTime/Class1.cs
@@ -1,127 +1,137 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using Fungus;
 using HarmonyLib;
+using KBEngine;
+using PaiMai;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using KBEngine;
 using UnityEngine;
 using UnityEngine.UI;
-using Fungus;
-using PaiMai;
 
 namespace Ventulus
 {
-    [BepInPlugin("Ventulus.MCS.BigTuPoSpendTime", "BigTuPoSpendTime", "1.0")]
+    [BepInPlugin("Ventulus.MCS.BigTuPoSpendTime", "大境界突破花费时间", "1.1")]
     public class BigTuPoSpendTime : BaseUnityPlugin
     {
         void Start()
         {
-            //输出日志
             Logger.LogInfo("大境界突破花费时间加载成功！");
+            ZhuJiHuiHeDays = Config.Bind<int>("config", "筑基每回合天数", 1, "默认1天");
+            JieDanHuiHeDays = Config.Bind<int>("config", "结丹每回合天数", 3, "默认3天");
+            JieYingHuiHeDays = Config.Bind<int>("config", "结婴每回合天数", 5, "默认5天");
+            HuaShenHuiHeDays = Config.Bind<int>("config", "化神每回合天数", 7, "默认7天");
+            DuJieHuiHeDays = Config.Bind<int>("config", "渡劫每回合天数", 9, "默认9天");
             var harmony = new Harmony("Ventulus.MCS.BigTuPoSpendTime");
             harmony.PatchAll();
         }
 
         public static BigTuPoSpendTime Instance;
+        public static ConfigEntry<int> ZhuJiHuiHeDays;
+        public static ConfigEntry<int> JieDanHuiHeDays;
+        public static ConfigEntry<int> JieYingHuiHeDays;
+        public static ConfigEntry<int> HuaShenHuiHeDays;
+        public static ConfigEntry<int> DuJieHuiHeDays;
         void Awake()
         {
             Instance = this;
         }
 
-        
+
         [HarmonyPatch(typeof(RoundManager))]
-        class RoundManagerPatch
+        class RoundManager_Patch
         {
             [HarmonyPostfix]
-            [HarmonyPatch("endRound")]
-            public static void Postfix(Entity _avater)
+            [HarmonyPatch(nameof(RoundManager.endRound))]
+            public static void endRound_Postfix(Entity _avater)
             {
                 Instance.Logger.LogInfo("大境界突破花费时间修改每回合");
                 //Instance.Logger.LogInfo(Tools.instance.monstarMag.FightType);
                 Avatar avatar = (Avatar)_avater;
                 if (Tools.instance.monstarMag.FightType == StartFight.FightEnumType.ZhuJi && avatar.isPlayer())
                 {
-                    avatar.AddTime(1, 0, 0);
+                    avatar.AddTime(ZhuJiHuiHeDays.Value, 0, 0);
                     Instance.Logger.LogInfo(avatar.worldTimeMag.nowTime);
                 }
                 if (Tools.instance.monstarMag.FightType == StartFight.FightEnumType.JieDan && avatar.isPlayer())
                 {
-                    avatar.AddTime(3, 0, 0);
+                    avatar.AddTime(JieDanHuiHeDays.Value, 0, 0);
                     Instance.Logger.LogInfo(avatar.worldTimeMag.nowTime);
                 }
                 if (Tools.instance.monstarMag.FightType == StartFight.FightEnumType.JieYing && avatar.isPlayer())
                 {
-                    avatar.AddTime(5, 0, 0);
+                    avatar.AddTime(JieYingHuiHeDays.Value, 0, 0);
                     Instance.Logger.LogInfo(avatar.worldTimeMag.nowTime);
-                }     
+                }
                 if (Tools.instance.monstarMag.FightType == StartFight.FightEnumType.HuaShen && avatar.isPlayer())
                 {
-                    avatar.AddTime(7, 0, 0);
+                    avatar.AddTime(HuaShenHuiHeDays.Value, 0, 0);
                     Instance.Logger.LogInfo(avatar.worldTimeMag.nowTime);
                 }
                 if (Tools.instance.monstarMag.FightType == StartFight.FightEnumType.DuJie && avatar.isPlayer())
                 {
-                    avatar.AddTime(9, 0, 0);
+                    avatar.AddTime(DuJieHuiHeDays.Value, 0, 0);
                     Instance.Logger.LogInfo(avatar.worldTimeMag.nowTime);
                 }
             }
         }
-            
+
 
 
         [HarmonyPatch(typeof(JieDanManager))]
-        class JieDanManagerPatch
+        class JieDanManager_Patch
         {
             [HarmonyPostfix]
-            [HarmonyPatch("JieSuan")]
-            public static void Postfix()
+            [HarmonyPatch(nameof(JieDanManager.JieSuan))]
+            public static void JieSuan_Postfix()
             {
                 Instance.Logger.LogInfo("大境界突破花费时间修改金丹提前完成");
                 Avatar player = Tools.instance.getPlayer();
                 //buff4010表示剩余回合，若结算时还有表示结丹提前完成
                 if (player.buffmag.HasBuff(4010))
                 {
-                    player.AddTime(3, 0, 0);
+                    player.AddTime(JieDanHuiHeDays.Value, 0, 0);
                     Instance.Logger.LogInfo(player.worldTimeMag.nowTime);
                 }
             }
         }
 
         [HarmonyPatch(typeof(JieYin))]
-        class JieYinPatch
+        class JieYin_Patch
         {
             [HarmonyPostfix]
-            [HarmonyPatch("JieYinSuccess")]
-            public static void JieYinSuccessPostfix()
+            [HarmonyPatch(nameof(JieYin.JieYinSuccess))]
+            public static void JieYinSuccess_Postfix()
             {
                 Instance.Logger.LogInfo("大境界突破花费时间修改结婴成功");
                 Avatar player = Tools.instance.getPlayer();
 
-                player.AddTime(5, 0, 0);
+                player.AddTime(JieYingHuiHeDays.Value, 0, 0);
                 Instance.Logger.LogInfo(player.worldTimeMag.nowTime);
             }
 
             [HarmonyPostfix]
-            [HarmonyPatch("JieYinFail")]
-            public static void JieYinFailPostfix()
+            [HarmonyPatch(nameof(JieYin.JieYinFail))]
+            public static void JieYinFail_Postfix()
             {
                 Instance.Logger.LogInfo("大境界突破花费时间修改结婴失败");
                 Avatar player = Tools.instance.getPlayer();
 
-                player.AddTime(5, 0, 0);
+                player.AddTime(JieYingHuiHeDays.Value, 0, 0);
                 Instance.Logger.LogInfo(player.worldTimeMag.nowTime);
             }
 
             [HarmonyPostfix]
-            [HarmonyPatch("JieYingYiZhiFail")]
-            public static void JieYingYiZhiFailPostfix()
+            [HarmonyPatch(nameof(JieYin.JieYingYiZhiFail))]
+            public static void JieYingYiZhiFail_Postfix()
             {
                 Instance.Logger.LogInfo("大境界突破花费时间修改结婴意志失败");
                 Avatar player = Tools.instance.getPlayer();
 
-                player.AddTime(5, 0, 0);
+                player.AddTime(JieYingHuiHeDays.Value, 0, 0);
                 Instance.Logger.LogInfo(player.worldTimeMag.nowTime);
             }
         }
