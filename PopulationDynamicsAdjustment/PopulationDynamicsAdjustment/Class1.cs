f00387051515a0c000e0b00724fd3862e32fee8f
Ventulus <everywherewind@163.com>
2023-03-04 04:14:12 +0800

    完成随机选取

--- PopulationDynamicsAdjustment/PopulationDynamicsAdjustment/Class1.cs
+++ PopulationDynamicsAdjustment/PopulationDynamicsAdjustment/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
+using Fungus;
 using GUIPackage;
 using HarmonyLib;
 using JSONClass;
@@ -11,9 +12,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -33,7 +34,7 @@ namespace Ventulus
         }
         void Start()
         {
-            Logger.LogInfo("加载成功！");
+            Logger.LogInfo("加载成功");
 
             var harmony = new Harmony("Ventulus.MCS.PopulationDynamicsAdjustment");
             harmony.PatchAll();
@@ -42,7 +43,7 @@ namespace Ventulus
         }
 
         public static PopulationDynamicsAdjustment Instance;
-        private DateTime LastJieSuanTime;
+        private static DateTime LastJieSuanTime;
         public static ConfigEntry<PopulationEnum> TargetPopulation;
         public enum PopulationEnum
         {
@@ -58,14 +59,18 @@ namespace Ventulus
             Much = 1800,
 
         }
+        //每年补充人数比例，数字越高补充越慢
+        private const int N = 4;
 
         private static int TotalPopulation;
-        private WeightDictionary NPCBigLevelStatistics;
-        private WeightDictionary NPCTypeStatistics;
+        private static WeightDictionary NPCBigLevelStatistics;
+        private static WeightDictionary NPCTypeStatistics;
 
-        private WeightDictionary NPCBigLevelTarget;
-        private WeightDictionary NPCTypeTarget;
-        private WeightDictionary NPCBigLevelAdjustment;
+        private static WeightDictionary NPCBigLevelTarget;
+        private static WeightDictionary NPCTypeTarget;
+        private static WeightDictionary NPCBigLevelAdjustment;
+        private static WeightDictionary NPCTypeAdjustment;
+        private static int PopulationAdjustment;
 
         [HarmonyPatch(typeof(NpcJieSuanManager))]
         class NpcJieSuanManager_Patch
@@ -74,8 +79,8 @@ namespace Ventulus
             [HarmonyPatch(nameof(NpcJieSuanManager.NpcJieSuan))]
             public static bool NpcJieSuan_Prefix()
             {
-                Instance.LastJieSuanTime = DateTime.Parse(NpcJieSuanManager.inst.JieSuanTime);
-                Instance.Logger.LogInfo("开始结算了！");
+                LastJieSuanTime = DateTime.Parse(NpcJieSuanManager.inst.JieSuanTime);
+                Instance.Logger.LogInfo("开始结算了");
                 Instance.Logger.LogInfo(NpcJieSuanManager.inst.JieSuanTime);
                 return true;
             }
@@ -84,7 +89,7 @@ namespace Ventulus
         public void AfterJieSuanStatistics(MessageData data = null)
         {
 
-            Logger.LogInfo("结算完成！");
+            Logger.LogInfo("结算完成");
             Logger.LogInfo(NpcJieSuanManager.inst.JieSuanTime);
             //每次结算统计
             StatisticsPopulation();
@@ -106,24 +111,60 @@ namespace Ventulus
 
         IEnumerator AdjustPopulation()
         {
-            DateTime tempdate = Instance.LastJieSuanTime;
+            DateTime tempdate = LastJieSuanTime;
             DateTime NowJieSuanTime = DateTime.Parse(NpcJieSuanManager.inst.JieSuanTime);
             while (NowJieSuanTime > RecentJune(tempdate, 6))
             {
 
-                Logger.LogMessage("经过六月份！");
+                Logger.LogMessage("经过六月份");
                 Logger.LogMessage(RecentJune(tempdate).ToString());
                 ///
                 yield return null;
+                //调查人口
                 StatisticsPopulation();
+                PopulationAdjustment = ((int)TargetPopulation.Value - TotalPopulation) / N + 1;
+                if (PopulationAdjustment <= 0)
+                {
+                    PopulationAdjustment = 0;
+                    Logger.LogMessage("无需调整");
+                    continue;
+                }
+
                 ///
                 yield return null;
+                //计算比例
                 NPCBigLevelTarget = new WeightDictionary(TargetBigLevelWeight, NPCBigLevel);
                 NPCTypeTarget = new WeightDictionary(TargetTypeWeight, NPCType);
-                Dictionary<int, float> subdict = NPCBigLevelTarget.PositiveSubtraction(NPCBigLevelStatistics.WeightDict);
+
+                Dictionary<int, double> subdict = NPCBigLevelTarget.PositiveSubtraction(NPCBigLevelStatistics.WeightDict);
                 NPCBigLevelAdjustment = new WeightDictionary(subdict, NPCBigLevel);
+
+                subdict = NPCTypeTarget.PositiveSubtraction(NPCTypeStatistics.WeightDict);
+                NPCTypeAdjustment = new WeightDictionary(subdict, NPCType);
+
+
+                Logger.LogInfo("调整境界比例");
+                Logger.LogInfo(NPCBigLevelAdjustment.ToString());
+
+                Logger.LogInfo("调整类型比例");
+                Logger.LogInfo(NPCTypeAdjustment.ToString());
+
+                Logger.LogInfo("本年调整人口");
+                Logger.LogInfo(PopulationAdjustment);
                 ///
                 yield return null;
+                //开始循环造人
+                for (int num = 1; num <= PopulationAdjustment; num++)
+                {
+                    int ChooseBigLevel = NPCBigLevelAdjustment.RollByWeight(out double roll);
+                    int ChooseType = NPCTypeAdjustment.RollByWeight(out double roll2);
+                    if (ChooseBigLevel <= 0 || ChooseType <= 0) continue;
+                    Logger.LogInfo($"{ChooseBigLevel}{NPCBigLevel[ChooseBigLevel]}+{ChooseType}{NPCType[ChooseType]}");
+
+
+                    ///
+                    //yield return null;
+                }
                 tempdate = tempdate.AddYears(1);
             }
             Logger.LogMessage("人口调整完成");
@@ -202,7 +243,7 @@ namespace Ventulus
             {24,"废弃"},
             {25,"万魂殿"},
         };
-        private static Dictionary<int, float> TargetTypeWeight = new Dictionary<int, float>()
+        private static Dictionary<int, double> TargetTypeWeight = new Dictionary<int, double>()
         {
             {1,80},
             {2,80},
@@ -238,7 +279,7 @@ namespace Ventulus
             {4,"元婴"},
             {5,"化神"},
         };
-        private static Dictionary<int, float> TargetBigLevelWeight = new Dictionary<int, float>()
+        private static Dictionary<int, double> TargetBigLevelWeight = new Dictionary<int, double>()
         {
             {1,120},
             {2,60},
@@ -250,7 +291,7 @@ namespace Ventulus
 
     public class WeightDictionary
     {
-        public Dictionary<int, float> WeightDict
+        public Dictionary<int, double> WeightDict
         {
             get;
             set;
@@ -260,33 +301,33 @@ namespace Ventulus
             get;
             set;
         }
-        private SortedDictionary<int, float> _sortDict;
-        private Dictionary<int, float> _percentDict;
+        private SortedDictionary<int, double> _sortDict;
+        private Dictionary<int, double> _percentDict;
 
         public WeightDictionary()
         {
-            WeightDict = new Dictionary<int, float>();
+            WeightDict = new Dictionary<int, double>();
             NameDict = new Dictionary<int, string>();
         }
-        public WeightDictionary(Dictionary<int, float> weightdict)
+        public WeightDictionary(Dictionary<int, double> weightdict)
         {
-            WeightDict = new Dictionary<int, float>(weightdict);
+            WeightDict = new Dictionary<int, double>(weightdict);
             NameDict = new Dictionary<int, string>();
         }
         public WeightDictionary(Dictionary<int, string> namedict)
         {
-            WeightDict = new Dictionary<int, float>();
+            WeightDict = new Dictionary<int, double>();
             NameDict = new Dictionary<int, string>(namedict);
         }
 
-        public WeightDictionary(Dictionary<int, float> weightdict, Dictionary<int, string> namedict)
+        public WeightDictionary(Dictionary<int, double> weightdict, Dictionary<int, string> namedict)
         {
-            WeightDict = new Dictionary<int, float>(weightdict);
+            WeightDict = new Dictionary<int, double>(weightdict);
             NameDict = new Dictionary<int, string>(namedict);
         }
         public WeightDictionary(WeightDictionary weightdictionary)
         {
-            WeightDict = new Dictionary<int, float>(weightdictionary.WeightDict);
+            WeightDict = new Dictionary<int, double>(weightdictionary.WeightDict);
             NameDict = new Dictionary<int, string>(weightdictionary.NameDict);
         }
         public void AddWeight(int key)
@@ -304,7 +345,7 @@ namespace Ventulus
         {
             StringBuilder SB = new StringBuilder();
             SB.Append(Environment.NewLine);
-            _sortDict = new SortedDictionary<int, float>(WeightDict);
+            _sortDict = new SortedDictionary<int, double>(WeightDict);
             foreach (var item in _sortDict)
             {
                 SB.Append("[");
@@ -322,26 +363,26 @@ namespace Ventulus
         {
             WeightDict = Normalization(WeightDict);
         }
-        public Dictionary<int, float> Normalization(Dictionary<int, float> weightdict)
+        public Dictionary<int, double> Normalization(Dictionary<int, double> weightdict)
         {
-            _sortDict = new SortedDictionary<int, float>();
-            float sum = weightdict.Values.Where(x => x > 0).Sum();
+            _sortDict = new SortedDictionary<int, double>();
+            double sum = weightdict.Values.Where(x => x >= 0).Sum();
             foreach (var item in weightdict)
             {
                 _sortDict.Add(item.Key, item.Value > 0 ? item.Value / sum : 0);
             }
-            return new Dictionary<int, float>(_sortDict);
+            return new Dictionary<int, double>(_sortDict);
         }
-        public Dictionary<int, float> PositiveSubtraction(Dictionary<int, float> percentdict2)
+        public Dictionary<int, double> PositiveSubtraction(Dictionary<int, double> percentdict2)
         {
             this.Normalization();
             _percentDict = Normalization(percentdict2);
-            _sortDict = new SortedDictionary<int, float>();
+            _sortDict = new SortedDictionary<int, double>();
             foreach (var item in WeightDict)
             {
                 if (_percentDict.ContainsKey(item.Key))
                 {
-                    float m = item.Value - _percentDict[item.Key];
+                    double m = item.Value - _percentDict[item.Key];
                     _sortDict.Add(item.Key, m > 0 ? m : 0);
                 }
                 else
@@ -349,7 +390,56 @@ namespace Ventulus
                     _sortDict.Add(item.Key, item.Value);
                 }
             }
-            return new Dictionary<int, float>(_sortDict);
+            if (_sortDict.Values.Where(x => x >= 0).Sum() <= 0)
+            {
+                return new Dictionary<int, double>(WeightDict);
+            }
+            return new Dictionary<int, double>(_sortDict);
+        }
+        private System.Random rnd = new System.Random();
+        public static long GetRandomLong()
+        {
+            byte[] array = new byte[8];
+            new RNGCryptoServiceProvider().GetBytes(array);
+            return BitConverter.ToInt64(array, 0);
+        }
+        public static double GetRandomDoubleRoll(double max)
+        {
+            if (max <= 0) return 0;
+            double result;
+            do
+            {
+                result = Math.Abs((double)GetRandomLong() / long.MaxValue);
+            } while (result >= max);
+            return result;
+        }
+        public double GetRandomDoubleRoll2(double max)
+        {
+            if (max <= 0) return 0;
+            double result;
+            do
+            {
+                result = rnd.NextDouble();
+            } while (result >= max);
+            return result;
+        }
+        public int RollByWeight(out double roll)
+        {
+            double sum = WeightDict.Values.Where(x => x >= 0).Sum();
+            roll = GetRandomDoubleRoll2(sum);
+            if (sum <= 0) return 0;
+
+            double countsum = 0;
+            foreach (var item in WeightDict)
+            {
+                if (item.Value <= 0) continue;
+                countsum += item.Value;
+                if (countsum > roll)
+                {
+                    return item.Key;
+                }
+            }
+            return 0;
         }
     }
 }
